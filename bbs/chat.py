b8db90379a4630068fd611bdcfb498709d02d07a
alex walters <1781587+tritium21@users.noreply.github.com>
2020-06-22 06:46:00 -0400

    Chat enhancement

--- bbs/chat.py
+++ bbs/chat.py
@@ -1,22 +1,30 @@
 import asyncio
 
+from colorama import Fore, Back, Style
+
 class Chat:
     def __init__(self):
         self._users = {}
 
     async def send(self, user, message):
         for recipient, client in self._users.items():
-            if recipient == user:
-                continue
-            await client.send(f"<{user.username}> {message}\r\n")
+            await client.recv(user, message)
 
     async def connect(self, user, reader, writer):
         if not user.can_chat and not user.is_admin:
             return 'You are banned from chat.'
+        await self.send(
+            user,
+            f"{Fore.RED}{Style.DIM}*** {user.username} has joined chat{Style.RESET_ALL}"
+        )
         self._users[user] = client = ChatClient(self, user, reader, writer)
         await client.chat()
         self._users.pop(user)
-        return ''
+        await self.send(
+            user,
+            f"{Fore.RED}{Style.DIM}*** {user.username} has left chat{Style.RESET_ALL}"
+        )
+        return 'Goodbye.'
 
 class ChatClient:
     def __init__(self, server, user, reader, writer):
@@ -33,17 +41,27 @@ class ChatClient:
             if line is self._reader.BREAK:
                 break
             line = line.rstrip()
-            if line == '/exit':
+            if line.startswith('/exit'):
                 self._writer.write('\r\n')
                 break
-            elif line:
-                await self._server.send(self._user, line)
-                await self.send(f'<{self._user.username}> {line}')
+            elif line.startswith('/me '):
+                _, _, remainder = line.partition(' ')
+                line = f"{Fore.GREEN}{Style.DIM}* {self._user.username} {remainder}{Style.RESET_ALL}"
+            else:
+                line = f"<{self._user.username}> {line}"
+            if line:
+                await self.send(line)
             self._writer.write('\r\x1b[2K' + prompt)
 
     async def send(self, message):
-        row, col = await self._reader.get_position()
+        await self._server.send(self._user, message)
+
+    async def print(self, message):
         self._writer.write(f'\x1b7\n\x1b[1A\x1b[1L{message}\x1b8')
+
+    async def recv(self, user, message):
+        await self.print(message)
+
 
 
 _chat = Chat()
